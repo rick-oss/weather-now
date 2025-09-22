c7e4c5d57632c205efef2077f55c2a0aab472692
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-09-22 20:06:46 -0300

    feat: render WeatherInfo component on main page

--- src/components/pages/WeatherPage.tsx
+++ src/components/pages/WeatherPage.tsx
@@ -1,7 +1,10 @@
 import styles from "./WeatherPage.module.css";
 
+import iconSunny from "../../assets/icon-sunny.webp";
+
 import Header from "../header/Header";
 import SearchBar from "../mainContent/SearchBar";
+import WeatherInfo from "../mainContent/WeatherInfo";
 
 function WeatherPage() {
   return (
@@ -10,6 +13,14 @@ function WeatherPage() {
       <h1>How's the sky looking today?</h1>
       <main className={styles.main_content}>
         <SearchBar />
+        <section className={styles.wrapper_content}>
+          <WeatherInfo
+            location="Berlin, Germany"
+            date="Tuesday, Aug 5, 2025"
+            temperature="20°"
+            temperature_icon={iconSunny}
+          />
+        </section>
       </main>
     </div>
   );
