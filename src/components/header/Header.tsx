80c170bc1e9391855e41e1f56192c14a120f2c8a
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-09-24 20:10:59 -0300

    feat: render unitsDropdown in Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,31 @@
+import { useState } from "react";
+
 import styles from "./Header.module.css";
 
 import logo from "../../assets/logo.svg";
 import iconUnits from "../../assets/icon-units.svg";
 import iconDropdown from "../../assets/icon-dropdown.svg";
 
+import UnitsDropdown from "../dropdown/UnitsDropdown";
+
 function Header() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
     <header className={styles.header}>
       <img src={logo} className={styles.image_logo} alt="Weather Now logo" />
-      <button className={styles.button_units}>
+      <button
+        className={styles.button_units}
+        onClick={() => {
+          setIsDropdownOpen(!isDropdownOpen);
+        }}
+      >
         <img src={iconUnits} alt="" />
         <span>Units</span>
         <img src={iconDropdown} alt="" />
       </button>
+
+      {isDropdownOpen && <UnitsDropdown isOpen={isDropdownOpen} />}
     </header>
   );
 }
