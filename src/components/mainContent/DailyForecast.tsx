fbd4ad16f0282ee6189ce7d84515b83d968339e5
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-09-30 14:54:30 -0300

    feat: feat: Make DailyForecast component data dynamic via API

--- src/components/mainContent/DailyForecast.tsx
+++ src/components/mainContent/DailyForecast.tsx
@@ -1,27 +1,26 @@
 import styles from "./DailyForecast.module.css";
 
-import iconRain from "../../assets/icon-rain.webp";
-import iconDrizzle from "../../assets/icon-drizzle.webp";
-import iconSunny from "../../assets/icon-sunny.webp";
-import iconPartlyCloud from "../../assets/icon-partly-cloudy.webp";
-import iconStorm from "../../assets/icon-storm.webp";
-import iconSnow from "../../assets/icon-snow.webp";
-import iconFog from "../../assets/icon-fog.webp";
-
 import DailyForecastCard from "../ui/DailyForecastCard";
 
+import { useWeather } from "../../context/WeatherContext";
+import { getWeatherIcon } from "../../utils/getWeatherIcon";
+
 function DailyForecast() {
+  const { dailyForecast } = useWeather();
+
   return (
     <section className={styles.daily_forecast}>
       <h3>Daily forecast</h3>
       <div className={styles.cards_daily_forecast}>
-        <DailyForecastCard day="Tue" icon={iconRain} temp_max={20} temp_min={14} />
-        <DailyForecastCard day="Wed" icon={iconDrizzle} temp_max={21} temp_min={15} />
-        <DailyForecastCard day="Thu" icon={iconSunny} temp_max={24} temp_min={14} />
-        <DailyForecastCard day="Fri" icon={iconPartlyCloud} temp_max={25} temp_min={13} />
-        <DailyForecastCard day="Sat" icon={iconStorm} temp_max={21} temp_min={15} />
-        <DailyForecastCard day="Sun" icon={iconSnow} temp_max={25} temp_min={16} />
-        <DailyForecastCard day="Mon" icon={iconFog} temp_max={24} temp_min={15} />
+        {dailyForecast?.days?.map((day, index) => (
+          <DailyForecastCard
+            key={index}
+            day={day.weekDay}
+            icon={getWeatherIcon(day.weatherCode)}
+            temp_max={day.max_temperature}
+            temp_min={day.min_temperature}
+          />
+        ))}
       </div>
     </section>
   );
