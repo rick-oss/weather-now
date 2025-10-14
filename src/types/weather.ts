f88aa25e31adf0056ee91ef1f645f2e1ac888fcf
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-10-14 17:53:25 -0300

    chore: remove obsolete fuction state

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -22,5 +22,4 @@ export interface WeatherApiProps {
   setRegion: Dispatch<SetStateAction<string | null>>;
   setIsLoading: Dispatch<SetStateAction<boolean>>;
   setError: Dispatch<SetStateAction<boolean>>;
-  setIsSearchProgress: Dispatch<SetStateAction<boolean>>;
 }
